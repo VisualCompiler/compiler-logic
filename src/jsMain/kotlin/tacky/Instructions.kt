e6fe56ecd4724b0692cff56860b6aa1dcbeef870
JoudiAlakkad <joudi.alakkad@outlook.com>
2025-08-19 14:29:43 +0200

    Add toJsonString for Tacky. Issue #18

--- src/jsMain/kotlin/tacky/Instructions.kt
+++ src/jsMain/kotlin/tacky/Instructions.kt
@@ -1,16 +1,58 @@
 package org.example.tacky
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 
 sealed class TackyInstruction : TackyConstruct()
 
 data class TackyRet(
     val value: TackyVal
-) : TackyInstruction()
+) : TackyInstruction() {
+    override fun toJsonString(): String {
+        val children =
+            JsonObject(
+                mapOf(
+                    "value" to JsonPrimitive(value.toJsonString())
+                )
+            )
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive("value"),
+                    "children" to children
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
 
 data class TackyUnary(
     val operator: TackyUnaryOP,
     val src: TackyVal,
     val dest: TackyVar
-) : TackyInstruction()
+) : TackyInstruction() {
+    override fun toJsonString(): String {
+        val children =
+            JsonObject(
+                mapOf(
+                    "operator" to JsonPrimitive(operator.name),
+                    "src" to JsonPrimitive(src.toJsonString()),
+                    "dest" to JsonPrimitive(dest.toJsonString())
+                )
+            )
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive("op, src, dest"),
+                    "children" to children
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
 
 enum class TackyUnaryOP {
     COMPLEMENT,
@@ -30,4 +72,25 @@ data class TackyBinary(
     val src1: TackyVal,
     val src2: TackyVal,
     val dest: TackyVar
-) : TackyInstruction()
+) : TackyInstruction() {
+    override fun toJsonString(): String {
+        val children =
+            JsonObject(
+                mapOf(
+                    "operator" to JsonPrimitive(operator.name),
+                    "src1" to JsonPrimitive(src1.toJsonString()),
+                    "src2" to JsonPrimitive(src2.toJsonString()),
+                    "dest" to JsonPrimitive(dest.toJsonString())
+                )
+            )
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive("op, src1, src2, dest"),
+                    "children" to children
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
--- src/jsMain/kotlin/tacky/Tacky.kt
+++ src/jsMain/kotlin/tacky/Tacky.kt
@@ -1,22 +1,92 @@
 package org.example.tacky
 
-sealed class TackyConstruct
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+
+sealed class TackyConstruct {
+    abstract fun toJsonString(): String
+}
 
 sealed class TackyVal : TackyConstruct()
 
 data class TackyConstant(
     val value: Int
-) : TackyVal()
+) : TackyVal() {
+    override fun toJsonString(): String {
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive(value.toString())
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
 
 data class TackyVar(
     val name: String
-) : TackyVal()
+) : TackyVal() {
+    override fun toJsonString(): String {
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive(name)
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
 
 data class TackyProgram(
     val function: TackyFunction
-) : TackyConstruct()
+) : TackyConstruct() {
+    override fun toJsonString(): String {
+        val children =
+            JsonObject(
+                mapOf(
+                    "function" to JsonPrimitive(function.toJsonString())
+                )
+            )
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive("function"),
+                    "children" to children
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
 
 data class TackyFunction(
     val name: String,
     val body: List<TackyInstruction>
-) : TackyConstruct()
+) : TackyConstruct() {
+    override fun toJsonString(): String {
+        // Map each instruction in the body to its JSON string
+        val bodyAsJson = JsonArray(body.map { JsonPrimitive(it.toJsonString()) })
+
+        val children =
+            JsonObject(
+                mapOf(
+                    "name" to JsonPrimitive(name),
+                    "body" to bodyAsJson
+                )
+            )
+        val jsonNode =
+            JsonObject(
+                mapOf(
+                    "type" to JsonPrimitive(this::class.simpleName),
+                    "label" to JsonPrimitive("name, body"),
+                    "children" to children
+                )
+            )
+        return Json.encodeToString(jsonNode)
+    }
+}
